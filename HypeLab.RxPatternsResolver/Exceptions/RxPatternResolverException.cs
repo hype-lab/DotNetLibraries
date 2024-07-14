b35652b15e0778283b7a245aa8c9c4ea8a1478d4
matt p <matteo.peru@live.com>
2024-07-14 19:22:04 +0200

    develop(gitignore added + new version builded)

---
+++ HypeLab.RxPatternsResolver/Exceptions/RxPatternResolverException.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace HypeLab.RxPatternsResolver.Exceptions
+{
+    /// <summary>
+    /// class
+    /// </summary>
+    public class RxPatternResolverException : Exception
+    {
+        /// <summary>
+        /// param
+        /// </summary>
+        public string? ResourceName { get; }
+
+        /// <summary>
+        /// param
+        /// </summary>
+        public ICollection<string>? Errors { get; }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public RxPatternResolverException() { }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message"></param>
+        public RxPatternResolverException(string? message)
+            : base(message) { }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="resourceName"></param>
+        public RxPatternResolverException(string? message, string resourceName) : base(message)
+        {
+            ResourceName = resourceName;
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        /// <param name="resourceName"></param>
+        /// <param name="errors"></param>
+        public RxPatternResolverException(string? message, Exception? innerException, string? resourceName, ICollection<string> errors) : base(message, innerException)
+        {
+            ResourceName = resourceName;
+            Errors = errors;
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public RxPatternResolverException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
